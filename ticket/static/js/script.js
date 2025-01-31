0426f42f046042ffb9a7c1d724347b7ed6092338
Eddy518 <edmwangi2@gmail.com>
2025-01-31 20:27:45 +0300

    fix: Fix toggling password visibility

--- ticket/static/js/script.js
+++ ticket/static/js/script.js
@@ -1,5 +1,6 @@
 const copyrightYear = document.querySelector("#copyright-year");
-const passwordField = document.querySelector(".password-input");
+const passwordFields = document.querySelectorAll("input[type='password']");
+const toggleButtons = document.querySelectorAll(".password-toggle-icon i");
 const confirmPasswordField = document.querySelector(".confirm-password-input");
 const togglePassword = document.querySelector(".password-toggle-icon i");
 const confirmTogglePassword = document.querySelector(
@@ -7,45 +8,36 @@ const confirmTogglePassword = document.querySelector(
 );
 const toastContainer = document.querySelector(".toast-container");
 const toastCloseButton = document.querySelector(".toast-btn");
+
 copyrightYear.textContent = (function () {
   return new Date().getFullYear();
 })();
 
-toastCloseButton.addEventListener("click", () => {
-  toastContainer.remove();
+if (toastContainer && toastCloseButton) {
+  toastCloseButton.addEventListener("click", () => {
+    toastContainer.remove();
+  });
+  setTimeout(() => {
+    toastContainer.remove();
+  }, 5000);
+}
+
+toggleButtons.forEach((toggle, index) => {
+  if (toggle) {
+    toggle.addEventListener("click", function () {
+      const passwordField = passwordFields[index];
+      if (passwordField.type === "password") {
+        passwordField.type = "text";
+        toggle.classList.remove("fa-eye");
+        toggle.classList.add("fa-eye-slash");
+      } else {
+        passwordField.type = "password";
+        toggle.classList.remove("fa-eye-slash");
+        toggle.classList.add("fa-eye");
+      }
+    });
+  }
 });
-
-setTimeout(() => {
-  toastContainer.remove();
-}, 5000);
-
-if (togglePassword) {
-  console.log("hey true");
-  togglePassword.addEventListener("click", function () {
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      togglePassword.classList.remove("fa-eye");
-      togglePassword.classList.add("fa-eye-slash");
-    } else {
-      passwordField.type = "password";
-      togglePassword.classList.remove("fa-eye-slash");
-      togglePassword.classList.add("fa-eye");
-    }
-  });
-}
-if (confirmTogglePassword) {
-  confirmTogglePassword.addEventListener("click", function () {
-    if (confirmPasswordField.type === "password") {
-      confirmPasswordField.type = "text";
-      confirmTogglePassword.classList.remove("fa-eye");
-      confirmTogglePassword.classList.add("fa-eye-slash");
-    } else {
-      confirmPasswordField.type = "password";
-      confirmTogglePassword.classList.remove("fa-eye-slash");
-      confirmTogglePassword.classList.add("fa-eye");
-    }
-  });
-}
 
 document.addEventListener("DOMContentLoaded", function () {
   //capture a user's timezone to a cookie
